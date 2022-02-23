b0577463f70e2cd816d84771291fad3496f38161
StalkerRaftik <altair-ivan@ya.ru>
2022-02-24 09:17:42 +1000

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,16 +1,1 @@
-@echo off
-cls
-echo Protecting srcds from crashes...
-echo If you want to close srcds and this script, close the srcds window and type Y depending on your language followed by Enter.
-title srcds.com Watchdog
-:srcds
-echo (%time%) srcds started.
-
-
-
-start /wait srcds.exe -console -game garrysmod +map gm_construct +maxplayers 16 +gamemode sandbox +r_hunkalloclightmaps 0
-
-
-
-echo (%time%) WARNING: srcds closed or crashed, restarting.
-goto srcds
+start srcds.exe -console -game garrysmod +map gm_construct +maxplayers 16 +gamemode sandbox +r_hunkalloclightmaps 0
